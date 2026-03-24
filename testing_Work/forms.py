c390665b6c1d111d58e90ea1e3c305b44909f326
Harishrivastav <harishrivastav544@gmail.com>
2026-03-24 12:32:14 +0530

    first commit

--- testing_Work/forms.py
+++ testing_Work/forms.py
@@ -1,62 +1,53 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import TestingJob
+
 
 class EngineerRegisterForm(UserCreationForm):
 
     email = forms.EmailField(
-        widget=forms.EmailInput(
-            attrs={'class':'form-control'}
-        )
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
     )
 
     username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={'class':'form-control'}
-        )
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
     password1 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={'class':'form-control'}
-        )
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
 
     password2 = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={'class':'form-control'}
-        )
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+
+    role = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
     class Meta:
-
         model = User
-
         fields = [
             'username',
             'email',
             'password1',
-            'password2'
+            'password2', 
+            'role'
         ]
 
-from .models import TestingJob
 
 class ProgressForm(forms.ModelForm):
 
     class Meta:
         model = TestingJob
-        fields = ['progress','status','report_file']
-
-
-from .models import TestingJob
+        fields = ['progress', 'status', 'report_file']
 
 
 class JobAssignForm(forms.ModelForm):
 
     class Meta:
-
         model = TestingJob
-
         fields = [
             'SRF',
             'job_no',
@@ -67,13 +58,6 @@ class JobAssignForm(forms.ModelForm):
         ]
 
         widgets = {
-
-            'received_date': forms.DateInput(
-                attrs={'type':'date'}
-            ),
-
-            'job_details': forms.Textarea(
-                attrs={'rows':3}
-            )
-
+            'received_date': forms.DateInput(attrs={'type': 'date'}),
+            'job_details': forms.Textarea(attrs={'rows': 3})
         }
